f7f3a55c3e125002eef73cc2d8bef4cd0e0dc41d
rniochy <lniochy@gmail.com>
2023-01-18 15:31:29 +0100

    added question file

---
+++ component/question/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../footer';
+import Header from '../header';
+import './quesion.css'
+
+const Question = () => {
+    return (
+        <div className='question-content'>
+        </div>
+    );
+}
+
+export default Question;
