22d4446f91ed5b106320200aacadd20671b4ebe0
rniochy <lniochy@gmail.com>
2023-01-20 14:28:25 +0100

    added GlobalStatus

--- store/Globalstatus.jsx
+++ store/Globalstatus.jsx
@@ -1,11 +1,12 @@
 import { useReducer } from 'react';
 import {createContext} from 'react';
+import { reducers } from './Reducers';
 
 export const DataContext = createContext();
 
  const DataProvider = ({children}) => {
      const inicialState = {}
-     const [status, dispatch] = useReducer()
+     const [status, dispatch] = useReducer(reducers, inicialState)
      return (
         <DataContext.Provider value={[status, dispatch]}>
             {children}
