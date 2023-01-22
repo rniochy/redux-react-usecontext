d76ed89aff516b35406dd5c5f01c1debd116c874
rniochy <lniochy@gmail.com>
2023-01-22 01:46:08 +0100

    Added Answer component

--- component/quiz/index.jsx
+++ component/quiz/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Answer from '../answer';
 import Question from '../question';
 
 const Quiz = () => {
     return (
         <div className='quiz-content content'>
             <Question/>
+            <Answer/>
         </div>
     );
 }
