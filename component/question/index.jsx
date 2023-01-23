e8dbf71a4738a53f9a21026a1096e7762701996c
rniochy <lniochy@gmail.com>
2023-01-23 07:41:29 +0100

    build the  question

--- component/question/index.jsx
+++ component/question/index.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import './question.css'
 
-const Question = () => {
+const Question = ({question}) => {
     return (
         <div className='question-content content'>
-            question
+             <section>
+                <div className='question-1 question'>
+                    <div>
+                        <span className='q-1'><h4>Q1</h4></span>
+                        <span className='q-text-1'>Something Question Here</span>
+                    </div>
+                </div>
+                <div className='question-2 question'>
+                    <div>
+                        <span className='q-text-1'>Something Question Here</span>
+                        <span className='q-2'><h4>Q2</h4></span>
+                    </div>
+                </div>
+                <div className='question-3 question' >
+                    <div>
+                        <span className='q-3'><h4>Q3</h4></span>
+                        <span className='q-text-3'>Something Question Here</span>
+                    </div>
+                </div>
+                <div className='question-4 question'>
+                    <div>
+                        <span className='q-text-4'>Something Question Here</span>
+                        <span className='q-4'><h4>Q4</h4></span>
+                    </div>
+                </div>
+             </section>
         </div>
     );
 }
--- component/quiz/index.jsx
+++ component/quiz/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Answer from '../answer';
 import Question from '../question';
-
+import './quiz.css'
 const Quiz = () => {
     return (
         <div className='quiz-content'>
