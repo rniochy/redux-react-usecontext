7b51ef1b60c90b803e6b02b2611d8dd639e84339
rniochy <lniochy@gmail.com>
2023-01-31 05:06:05 +0100

    Fixing bug into reducers function

--- store/Reducers.jsx
+++ store/Reducers.jsx
@@ -2,22 +2,22 @@ import actions from "./Actions";
 import questions from '../data/data';
 
 export const reducers  = (state, action)=>{
-        const inicialState = {questions, current:0, isNotFinal: true};
+  
      console.log(state, action)
         switch(action.type){
         case actions.next_question:{
                 
             return  {
-                ...state, current: action.payload.current, isNotFinal: action.payload.isNotFinal
+                  ...state, current: action.payload.current, isNotFinal: action.payload.isNotFinal
                 }
         }
         case actions.next_question: {
+                state.current = 0; state.isNotFinal = true;
                return { 
-                   ...state, 
+                   ...state, current: action.payload.current, isNotFinal: action.payload.isNotFinal
                 }
         } 
         default: {
-
                 return state;
         }
 
