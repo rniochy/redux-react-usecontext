af129386fe6a56d0aecb427836d2bbb466f7f824
rniochy <lniochy@gmail.com>
2023-02-06 09:26:23 +0100

    Adding timer

--- component/header/index.jsx
+++ component/header/index.jsx
@@ -3,12 +3,23 @@ import './header.css'
 const Header = ({score}) => {
     const {left, remain} = score;
     return (
+     <>
         <div className='header-content content'>
-            <h2>Show what you know</h2>
-            <p>Choose and select your answer below</p>
-            <p>Question number {left} rest {remain - left} question</p>   
-            <p>total: {remain}</p>
+            <div>
+                <h2>Show what you know</h2>
+                <p>Choose and select your answer below</p>
+                <p>Question number {left} rest {remain - left} question</p>   
+                <p>total: {remain}</p>
+            </div>
+            <div className='header-timer'>
+                <span className='header-timer-number'>
+                    <span>Left time</span>
+                    <span>30</span>
+                
+                </span>
+            </div>
         </div>
+     </>
     );
 }
 export default Header;
